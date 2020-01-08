b5ac3d6f156de8cc5c68184b595f16d0c1e3919e
Aymeric DUJARDIN <aymeric.dujardin@stereolabs.com>
2020-01-08 16:06:23 +0100

    Add Jetson JP4.3

---
+++ .build_jetson_images.sh
@@ -0,0 +1,18 @@
+cd 2.8/l4t/jetpack_4.2/devel
+docker build -t stereolabs/zed:2.8-devel-jetson-jp4.2.1 .
+# aliases
+docker build -t stereolabs/zed:2.8-devel-l4t-r32.2.1 . 
+docker build -t stereolabs/zed:2.8-devel-jetson-jp4.2 .
+
+docker push stereolabs/zed:2.8-devel-jetson-jp4.2.1
+docker push stereolabs/zed:2.8-devel-jetson-jp4.2
+docker push stereolabs/zed:2.8-devel-l4t-r32.2.1
+
+cd ../../jetpack_4.3/devel
+docker build -t stereolabs/zed:2.8-devel-jetson-jp4.3 .
+# aliases
+docker build -t stereolabs/zed:2.8-devel-l4t-r32.3.1 . 
+
+docker push stereolabs/zed:2.8-devel-jetson-jp4.3
+docker push stereolabs/zed:2.8-devel-l4t-r32.3.1
+cd ../../../../
